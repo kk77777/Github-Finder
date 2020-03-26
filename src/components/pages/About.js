2475f6b42a9f0e389980d720f078ab8d4f8f0fc4
kk77777 <kaushikgattani@gmail.com>
2020-03-26 14:18:59 +0530

    created about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About Page</h1>
+      <p>This is an app to search for github users.</p>
+    </Fragment>
+  );
+};
+
+export default About;
