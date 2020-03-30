b2927ae99a4bb8c5a98178c200b35d0adb9ec3e5
kk77777 <kaushikgattani@gmail.com>
2020-03-30 08:41:12 +0530

    added NotFound component

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404 Not found</h1>
+      <p>Sorry, but the page you are looking for does not exists.</p>
+    </div>
+  );
+}
+
+export default NotFound;
